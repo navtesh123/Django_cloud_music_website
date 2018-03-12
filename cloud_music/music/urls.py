9f580d9a580c5dd29b6d78fbb3b9696700d019ce
navteshdhir123 <navteshdhir123@gmai.com>
2018-03-13 01:36:23 +0530

    function views replaced by generic views

--- cloud_music/music/urls.py
+++ cloud_music/music/urls.py
@@ -1,22 +1,13 @@
 from django.urls import path
-from . import views  #. means the sme dir
+from . import views
 
 #specify to which app does this url file is asociated with
 app_name='music'
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('',views.IndexView.as_view(),name='index'),
     #music/71/
-    path('<album_id>/',views.detail, name='detail'),
-    #sometimes the url does not  only directs you
-    #to a differtent page, rather it preforms some
-    #task and then take you to the different page.
-                 #for example
-    #the logout system:if you click logout,
-    #it first,logs you out
-    #then, directs you to a page(homepage)
-
-    #music/71/favourite
-    path('<album_id>/favourite/',views.favourite, name='favourite'),
-
-]
+    path('<pk>/',views.DetailView.as_view(),name='detail'),
+    #music/album/add/
+    path('album/add/',views.AlbumCreate.as_view(),name='album-add'),
+    ]
--- cloud_music/music/views.py
+++ cloud_music/music/views.py
@@ -1,37 +1,18 @@
-#    try:
-#        album=Album.objects.get(pk=album_id)
-#    except Album.DoesNotExist:
-#        raise Http404("album does not exist")
-#    return render(request,'music/detail.html',{'album':album})
+from django.views import generic
+from .models import Album
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
 
-#***all of the above code can be shortned by importing get_objects_or_404
-#***from django.shortcuts
+class IndexView(generic.ListView):
+    context_object_name='all_albums'
+    template_name='music/index.html'
 
-from django.shortcuts import render, get_object_or_404
-from .models import Album, Song
+def get_queryset(self):
+        return Album.objects.all()
 
+class DetailView(generic.DetailView):
+    model=Album
+    template_name='music/detail.html'
 
-def index(request):
-    all_albums = Album.objects.all()
-    return render(request, 'music/index.html', {'all_albums': all_albums})
-
-
-def detail(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album': album})
-
-
-def favourite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except (KeyError, Song.DoesNotExist):
-         return render(request, 'music/detail.html', {
-             'album': album,
-             'error_message': "No song selected",
-        })
-
-    else:
-        selected_song.is_favourite = True
-        selected_song.save()
-    return render(request, 'music/detail.html', {'album': album})
+class AlbumCreate(CreateView):
+    model:Album
+    fields:['artist','album_title','genre','album_logo']
